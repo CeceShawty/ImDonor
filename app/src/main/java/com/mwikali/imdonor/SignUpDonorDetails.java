1c2acdd7efd55add889b4438bc2b6119fdbc19e9
CeceShawty <Mutuacynthia04@gmail.com>
2019-10-24 15:33:56 +0300

    initial commit

---
+++ app/src/main/java/com/mwikali/imdonor/SignUpDonorDetails.java
@@ -0,0 +1,14 @@
+package com.mwikali.imdonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpDonorDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_donor_details);
+    }
+}
---
+++ app/src/main/java/com/mwikali/imdonor/SingUpActivity.java
@@ -0,0 +1,34 @@
+package com.mwikali.imdonor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    private boolean isDonor = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        isDonor = getIntent().getExtras().getBoolean("isDonor");
+
+        TextView tvSignUp = findViewById(R.id.txt_signup);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isDonor) {
+                    startActivity(new Intent(SingUpActivity.this, SignUpDonorDetails.class));
+                } else {
+                    startActivity(new Intent(SingUpActivity.this, SignUpBankDetails.class));
+                }
+            }
+        });
+
+    }
+}
